d79f58d720759b8329da31fdbcf4b7a23578f4a1
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 17:09:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   def execute_transaction
     if valid? && sender.balance > amount && self.status = "pending"
       sender.balance -= amount
-      recei.balance += amount
+      receiver.balance += amount
       self.status = "complete"
     else
       reject_transfer
