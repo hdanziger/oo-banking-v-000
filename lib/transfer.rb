8171a4401d17e94ca3e01e319e423c6c55bc5fc7
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 16:58:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
+  attr_accessor :status, :transfer, :sender, :reciever, :transfer_amount
+  attr_reader :name
   # your code here
+  def initialize
+    @status = "pending"
+    @transfer = transfer
+    @sender = sender
+    @reciever = reciever
+    @transfer_amount = transfer_amount
 end
